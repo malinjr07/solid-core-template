4c947cf3492249102a29ccadd6fa22c03fa7378c
Mosaddik Ali Maruf <malinjr07@gmail.com>
2026-01-11 08:32:21 +0600

    ci: Commit lint limit increased

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -3,13 +3,13 @@ module.exports = {
   rules: {
     'body-leading-blank': [2, 'always'],
     'body-min-length': [2, 'always', 20],
-    'body-max-line-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 250],
     'header-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72],
     'subject-case': [
       2,
       'always',
-      ['sentence-case', 'start-case', 'pascal-case']
+      ['sentence-case', 'start-case', 'pascal-case'],
     ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
@@ -26,13 +26,13 @@ module.exports = {
         'chore',
         'perf',
         'ci',
-        'revert'
-      ]
+        'revert',
+      ],
     ],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'scope-case': [2, 'always', 'lower-case'],
-    'footer-leading-blank': [1, 'always']
+    'footer-leading-blank': [1, 'always'],
   },
   helpUrl:
     'Please follow the commit message format:\n' +
@@ -40,5 +40,5 @@ module.exports = {
     'Body (min 20 chars)\n\n' +
     'Allowed Types: feat, fix, docs, style, refactor, test, chore, perf, ci, revert\n' +
     'Example: feat(auth): Add user authentication\n\n' +
-    'This is a more detailed explanation of the changes made in this commit.'
+    'This is a more detailed explanation of the changes made in this commit.',
 };
